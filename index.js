e6529d619cc52e4e52644048f22328fd65f9fb7d
Julio Litzenberg <julio.litzenberg@gmail.com>
2022-03-19 17:39:33 +0100

    Adding views and template engine

--- index.js
+++ index.js
@@ -4,7 +4,28 @@ const koa = require("koa");
 const port = process.env.PORT || 3001;
 const server = new koa();
 const static = require("koa-static");
+const Router = require("koa-router");
+const views = require("koa-views");
 
+const route = new Router();
+
+// Routes
+//route.get("/", (ctx, next) => (ctx.body = "Hola Mundo"));
+route.get("/", (ctx, next) => {
+  return ctx.render("./index.html", {
+    name: "Julio Litzenberg",
+  });
+});
+
+route.get("/:name/:age", (ctx, next) => {
+  return ctx.render("./index.html", {
+    name: ctx.params.name,
+    age: ctx.params.age,
+  });
+});
+
+server.use(views("./views", { map: { html: "nunjucks" } }));
+server.use(route.routes());
 server.use(static("./public"));
 
 server.listen(port, () => {
